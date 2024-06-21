da2431fe39005c013bf246720d4455576bc1cf33
yui151cm <167074454+yui151cm@users.noreply.github.com>
2024-06-21 17:17:15 +0900

    Create quiz.php

---
+++ quiz.php
@@ -0,0 +1,5 @@
+<form action="quiz.php" method="get">
+  <label for="username">Enter your name:</label>
+  <input type="text" id="24G3035&teacher=yabuki" name="username" required>
+  <input type="submit" value="Submit">
+</form>
