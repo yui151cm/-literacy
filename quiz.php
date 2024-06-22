cc009444010c2796be5d5c6e747bc4358c3492ca
yui151cm <167074454+yui151cm@users.noreply.github.com>
2024-06-22 09:47:24 +0900

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,5 +1,4 @@
 <form action="puiz.php" method="get"/>
 <label for="username">Enter your name:</label>
 <input type="text" id=24G3035&teacher" name="username"/>
-<input type="submit" value="Submit"/>
 </form>
